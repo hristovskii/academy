2da8fd1cc8d293d5929c3402507e604239c11583
Petar Hristovski <petarhristovski23@gmail.com>
2024-05-06 00:47:46 +0200

    adding sponsors

--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -2,21 +2,41 @@
 
 import Image from "next/image";
 import dose from "@/app/resources/dose.png";
-import HTec from "@/app/resources/logo/vectorhtec-logo.svg";
+import kvatro from "@/app/resources/kvatro.webp";
+import morino from "@/app/resources/Morino.webp";
+import pekara from "@/app/resources/Pekarata_od_karshi.webp";
 import Heading from "./Heading";
 import { RevealList } from  'next-reveal'
 
 export default function Sponsors() {
     return(
-        <section className="md:mx-6 mx-auto lg:px-36 md:px-16 px-8 mt-12 mb-20">
-            <RevealList interval={100} delay={500}>
+        <section className="my-10 md:mx-28 mx-12">
             <div className="lg:px-36 md:px-16 px-4 grid justify-center items-center">
-                <Heading heading={"Partners & Supporters"}></Heading>
-                <Image src={dose} alt="Sponsor Logo" width={600} className="mt-8 mb-8"></Image>
-                {/* <Image src={CrazyLabs} alt="Sponsor Logo" height={250} className="mt-8 mb-8"></Image>
-                <Image src={HTec} alt="Sponsor Logo" height={250} className="mt-8 mb-8"></Image> */}
+            <Heading heading={"Organizators for the coming event"}></Heading>
             </div>
+            
+            <div className="lg:flex justify-evenly items-center gap-14">
+            <RevealList interval={100} delay={500}>
+                <div className="text-eestec text-center items-center" >
+                <Image src={dose} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+                
+                <div className="text-eestec text-center items-center" >
+                <Image src={kvatro} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+
+                <div className="text-eestec text-center items-center" >
+                <Image src={morino} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+
+                <div className="text-eestec text-center items-center" >
+                <Image src={pekara} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+
             </RevealList>
+            </div>
+
+            
         </section>
     );
 }
