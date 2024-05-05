479ca05124f5e2f3d432962b3584cf358aa3dddc
Petar Hristovski <petarhristovski23@gmail.com>
2024-05-06 01:21:33 +0200

    adding sponsors

--- src/app/components/Sponsors.tsx
+++ src/app/components/Sponsors.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Image from "next/image";
+import tmf from "@/app/resources/tmf.jpeg";
+import finki from "@/app/resources/finki.png";
 import dose from "@/app/resources/dose.png";
 import kvatro from "@/app/resources/kvatro.webp";
 import morino from "@/app/resources/Morino.webp";
@@ -13,13 +15,33 @@ export default function Sponsors() {
         <section className="my-10 md:mx-28 mx-12">
             <RevealList interval={100} delay={500}>
             <div className="lg:px-36 md:px-16 px-4 grid justify-center items-center">
-            <Heading heading={"Sponsors & Supporters "}></Heading>
+            <Heading heading={"Our Supporters"}></Heading>
             </div>
             
             <div className="lg:flex justify-evenly items-center gap-14">
             
                 <div className="text-eestec text-center items-center" >
-                <Image src={dose} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                <Image src={finki} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+                
+                <div className="text-eestec text-center items-center" >
+                <Image src={tmf} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                </div>
+
+            </div>
+
+            </RevealList>
+
+
+            <RevealList interval={100} delay={500}>
+            <div className="lg:px-36 md:px-16 px-4 grid justify-center items-center">
+            <Heading heading={"Our Sponsors"}></Heading>
+            </div>
+            
+            <div className="lg:flex justify-evenly items-center gap-14">
+            
+                <div className="text-eestec text-center items-center" >
+                <Image src={morino} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
                 </div>
                 
                 <div className="text-eestec text-center items-center" >
@@ -27,11 +49,11 @@ export default function Sponsors() {
                 </div>
 
                 <div className="text-eestec text-center items-center" >
-                <Image src={morino} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                <Image src={pekara} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
                 </div>
 
                 <div className="text-eestec text-center items-center" >
-                <Image src={pekara} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
+                <Image src={dose} alt="Sponsor Logo" height={300} className="mt-8 mb-8"></Image>
                 </div>
 
             </div>
